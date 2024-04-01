5ee6bf7a2e6a7a7b8386246101acc59b0233168c
sujitdhamke33 <153482426+sujitdhamke33@users.noreply.github.com>
2024-04-01 07:17:36 +0530

    Update evenLocationElement.java

--- evenLocationElement.java
+++ evenLocationElement.java
@@ -2,7 +2,7 @@ package Strings;
 
 public class evenLocationElement {
     public static void main(String[] args) {
-        String [] array = {"sujit","ram","babu","Sona","mona"};
+        String [] array = {"sujit","ram","babu","Shubham","monali"};
         for (int i = 0; i < array.length; i+=2) {
             System.out.println(array[i]);
         }
